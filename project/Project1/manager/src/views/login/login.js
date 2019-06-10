33729941cb38e60c36ae80c86118da2a2b106ade
a5354159 <390354259@qq.com>
2019-06-10 19:11:37 +0800

    登录完成排版

--- project/Project1/manager/src/views/login/login.js
+++ project/Project1/manager/src/views/login/login.js
@@ -9,24 +9,22 @@ class Login extends Component {
   render() {
     return (
       <div className={styles.main}>
-        <div class={styles.login_wraper}>
-          <div class="login-content">
-            <div class="iptbox user_name">
-              <input
-                className="ipt"
-                placeholder="请输入用户名"
-                onchange="add"
-              />
-            </div>
-            <div class="iptbox pwd">
-              <input class="ipt" placeholder="请输入密码" />
-            </div>
-            <div class="text-area">
-              <span></span>
-              <span>忘记密码</span>
-            </div>
-            <button className="btns">登录</button>
+        <div className={styles.login_wraper}>
+          <div className={styles.iptbox}>
+            <input
+              className={styles.ipt}
+              placeholder="请输入用户名"
+              onchange="add"
+            />
           </div>
+          <div className={styles.iptbox}>
+            <input className={styles.ipt} placeholder="请输入密码" />
+          </div>
+          <div className={styles.textArea}>
+            <span />
+            <span>忘记密码</span>
+          </div>
+          <button className={styles.loginBtn}>登录</button>
         </div>
       </div>
     );
