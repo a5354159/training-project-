90ff7a67f18715e78d80fcb9bd18d25de5116c30
a5354159 <390354259@qq.com>
2019-06-12 20:08:59 +0800

    上传完整登录

--- project/Project1/manager/src/index.js
+++ project/Project1/manager/src/index.js
@@ -1,5 +1,10 @@
+import '@babel/polyfill';
 import dva from 'dva';
+// 引入全局样式
 import './index.css';
+// 引入antd样式
+import 'antd/dist/antd.css';
+
 // 1. Initialize
 const app = dva();
 
@@ -7,7 +12,7 @@ const app = dva();
 // app.use({});
 
 // 3. Model
-// app.model(require('./models/example').default);
+app.model(require('./models/user').default);
 
 // 4. Router
 app.router(require('./router').default);
